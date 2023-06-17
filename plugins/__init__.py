2a56bdf3eede1a5e39e2fb5fecda861bb9569596
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-06-17 10:33:19 +0530

    Add files via upload

---
+++ plugins/__init__.py
@@ -0,0 +1,15 @@
+#(©)harshil8981
+#@iryme
+
+
+
+
+
+from aiohttp import web
+from .route import routes
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
---
+++ plugins/route.py
@@ -0,0 +1,14 @@
+#(©)harshil8981
+#rymme
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("CodeXBotz")
